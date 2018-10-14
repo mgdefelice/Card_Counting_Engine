14172a23c645efe82497a5ed28cbd5dc21c5e764
mgdefelice <marcello.defelice@gmail.com>
2018-10-14 13:57:36 -0400

    Updates

---
+++ main.js
@@ -0,0 +1,43 @@
+let count = 0;
+let betOrHold = "Hold Bets";
+var theCount = 0;
+
+document.getElementById("theCount").innerHTML = betOrHold;
+
+function countingCards(card) {
+  switch (card) {
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+      count++;
+      break;
+
+    case 10:
+    case "Jack":
+    case "Queen":
+    case "King":
+    case "Ace":
+      count--;
+      break;
+    case 19:
+      count = 0;
+  }
+
+  if (count >= 0) {
+    betOrHold = count + " Low Bets";
+    document.getElementById("theCount").innerHTML = betOrHold;
+    if (count > 5) {
+      betOrHold = count + " High Bets, VERY HOT";
+      document.getElementById("theCount").innerHTML = betOrHold;
+    }
+  } else if (count <= 0) {
+    betOrHold = count + "Low Bets";
+    document.getElementById("theCount").innerHTML = betOrHold;
+    if (count < -5) {
+      betOrHold = count + "Low Bets, VERY COLD";
+      document.getElementById("theCount").innerHTML = betOrHold;
+    }
+  }
+}
